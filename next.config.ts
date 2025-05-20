899cf25e8255574e5133e9b0a39adcab3a3f4c9a
jeffrey <jeffrey@infloww.com>
2025-05-20 11:41:18 +0800

    111

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const isLocal = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
   output: isLocal ? undefined : "export",
   reactStrictMode: true,
+  basePath: `/${process.env.version}`,
+  assetPrefix: `/${process.env.version}/`,
   images: {
     unoptimized: true,
   },
