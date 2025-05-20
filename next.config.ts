1c1f454d6e8b0d7d3c03caf33e8a35d0113361cb
jeffrey <jeffrey@infloww.com>
2025-05-20 11:30:19 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-// const repoName = "live-server";
+const isLocal = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: isLocal ? undefined : "export",
   reactStrictMode: true,
-  // basePath: `/${repoName}`,
-  // assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={`${styles.page}`}>
+        <div>{process.env.NODE_ENV}——{process.env.NEXT_PUBLIC_ENV_NAME}</div>
         <main className={styles.main}>
           <Image
             className={styles.logo}
